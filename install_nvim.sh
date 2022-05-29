f5e402d29ae69bac1bbd4f945430f6aad413fe21
SyStem <g1-assassin@hotmail.com>
2022-05-29 16:47:34 +0200

    Updates

--- install_nvim.sh
+++ install_nvim.sh
@@ -16,15 +16,10 @@ else
     cp -r $ABSOLUTE_PATH/nvim ~/.config/
 fi
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Installing the neovim plugin manager - https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "MAKE SURE TO RUN :PlugInstall inside of neovim on first launch."
 echo "Configuration complete"
-
-
-
-
-
-
 
